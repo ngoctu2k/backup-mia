bd6a507cc79ad428d6e13c56a3cb9b19037d4094
tran tu <ongchukim2k@gmail.com>
2020-09-13 09:56:55 +0700

    updtae region

--- src/app/pages/fruit/services/fruit.service.ts
+++ src/app/pages/fruit/services/fruit.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BaseApiService } from 'src/app/shared/services/base.service';
 import { Fruit } from '../models/fruit.model';
 
@@ -10,6 +11,6 @@ export class FruitService extends BaseApiService<Fruit>{
     super(http, "api/product")
   }
   getDetaiFruit():Observable<any>{
-      return this.http.get("5");
+      return this.http.get("region").pipe(map((data:any) => data.Payload));
   }
 }
