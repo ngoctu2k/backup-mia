1199b090b6e4b95af968eea88267bc9fd09ddd20
Thanh Phong <phonga3bdq@gmail.com>
2020-09-13 08:52:21 +0700

    fix

--- src/app/pages/fruit/services/fruit.service.ts
+++ src/app/pages/fruit/services/fruit.service.ts
@@ -3,13 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BaseApiService } from 'src/app/shared/services/base.service';
 import { Fruit } from '../models/fruit.model';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({providedIn:"root"})
 export class FruitService extends BaseApiService<Fruit>{
   constructor(public http:HttpClient){
     super(http, "api/product")
   }
-  getDetaiFruit():Observable<any>{
-      return this.http.get("5");
+  getDetailFruit():Observable<any>{
+      return this.http.get("api/products/province/1").pipe(map((data:any) => data.Payload));
   }
 }
