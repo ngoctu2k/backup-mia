c2c267679e3d319fe355addd80c60256d0dc0371
Thanh Phong <phonga3bdq@gmail.com>
2020-09-13 14:40:19 +0700

    fix

--- src/app/pages/fruit/components/fruit-nation/nation-overview/nation-overview.component.ts
+++ src/app/pages/fruit/components/fruit-nation/nation-overview/nation-overview.component.ts
@@ -18,6 +18,17 @@ export class NationOverviewComponent implements OnInit {
   @Input() isDetail: boolean;
   @Output() showFruitDetail = new EventEmitter<boolean>();
   @Input() selectedIndex: number;
+  Region = [
+    {id: 1, name: "Việt Nam", slug:"viet-nam"},
+    {id: 2, name: "Đông Bắc Bộ", slug:"dong-bac-bo"},
+    {id: 3, name: "Tây Bắc", slug: 'tay-bac'},
+    {id: 4, name: "Đông bằng sông Hồng", slug:'dong-bang-song-hong'},
+    {id: 5, name: "Bắc Trung Bộ", slug:'bac-trung-bo'},
+    {id: 6, name: "Nam Trung Bộ", slug:'nam-trung-bo'},
+    {id: 7, name: "Tây Nguyên", slug:'tay-nguyen'},
+    {id: 8, name: "Đông Nam Bộ", slug:'dong-nam-bo'},
+    {id: 9, name: "Đồng bằng sông Cửu Long", slug:'dong-bang-song-cuu-long'}
+]
   public regionConf: PaginationInstance = {
     id: 'region',
     itemsPerPage: 5,
@@ -36,7 +47,7 @@ export class NationOverviewComponent implements OnInit {
     1, 2, 3, 4, 5, 6,
   ];
   list_region;
-
+  route;
 
   constructor(private regionService: RegionService) { }
 
