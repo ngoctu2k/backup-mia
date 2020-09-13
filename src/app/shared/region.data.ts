e34b643e3d22ba4eb1f9643c7c14f0dfa098bc31
Thanh Phong <phonga3bdq@gmail.com>
2020-09-13 16:29:21 +0700

    fix

---
+++ src/app/shared/region.data.ts
@@ -0,0 +1,11 @@
+export const Region = [
+  {id: 1, name: "Việt Nam", slug:"viet-nam"},
+  {id: 2, name: "Đông Bắc Bộ", slug:"dong-bac-bo"},
+  {id: 3, name: "Tây Bắc", slug: 'tay-bac'},
+  {id: 4, name: "Đông bằng sông Hồng", slug:'dong-bang-song-hong'},
+  {id: 5, name: "Bắc Trung Bộ", slug:'bac-trung-bo'},
+  {id: 6, name: "Nam Trung Bộ", slug:'nam-trung-bo'},
+  {id: 7, name: "Tây Nguyên", slug:'tay-nguyen'},
+  {id: 8, name: "Đông Nam Bộ", slug:'dong-nam-bo'},
+  {id: 9, name: "Đồng bằng sông Cửu Long", slug:'dong-bang-song-cuu-long'}
+]
